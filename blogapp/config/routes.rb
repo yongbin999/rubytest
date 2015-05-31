8c6b5f52708c7507cda17006f86d11840a44b34c
yongbin999 <yongbinliang@umass.edu>
2015-05-31 10:40:50 -0400

    routes and views and validation for articles and update function

--- blogapp/config/routes.rb
+++ blogapp/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   resources :articles
   # You can have the root of your site routed with "root"
-  root 'welcome#hello'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
