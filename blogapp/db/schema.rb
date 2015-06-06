dd2f2d689b43309c905e854ce61c8c35b30a1444
yongbin999 <yongbinliang@umass.edu>
2015-06-05 23:21:18 -0400

    update new app views

--- blogapp/db/schema.rb
+++ blogapp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605010831) do
+ActiveRecord::Schema.define(version: 20150606031341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,22 @@ ActiveRecord::Schema.define(version: 20150605010831) do
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people_admins", force: :cascade do |t|
+    t.string   "name"
+    t.string   "school"
+    t.string   "department"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people_students", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "grade"
+    t.boolean  "attendence"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
